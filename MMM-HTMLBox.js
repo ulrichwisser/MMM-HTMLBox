62829bf615dac83939b7b78ee8d7469685b26e2a
eouia <eouia0819@gmail.com>
2017-11-08 10:08:45 +0100

    add file and interval function

--- MMM-HTMLBox.js
+++ MMM-HTMLBox.js
@@ -4,9 +4,58 @@
 * By eouia
 */
 Module.register("MMM-HTMLBox",{
-  getDom: function() {
-    var wrapper = document.createElement("div")
-    wrapper.innerHTML = this.config.content
-    return wrapper
-  }
+	defaults: {
+		width: "100%",
+		height: "inherit",
+		refresh_interval_sec: 0,
+		content: "There is nothing to display. <br>Put your HTML code into content field in 'config.js'.",
+		file: "sample.html",
+	},
+
+	start: function() {
+		this.timer = null
+
+	},
+
+	notificationReceived: function(noti, payload, sender) {
+		if (noti == "DOM_OBJECTS_CREATED") {
+			this.refresh()
+		}
+	},
+
+	refresh: function() {
+		if (this.config.file !== "") {
+			this.readFileTrick("/modules/MMM-HTMLBox/" + this.config.file)
+		}
+		this.updateDom()
+		if (this.config.refresh_interval_sec > 0) {
+			var self = this
+			this.timer = setTimeout(function(){
+				self.refresh()
+			}, this.config.refresh_interval_sec * 1000)
+		}
+	},
+
+	getDom: function() {
+		var wrapper = document.createElement("div")
+		wrapper.innerHTML = this.config.content
+		wrapper.className = "HTMLBX"
+		wrapper.style.width = this.config.width
+		wrapper.style.height = this.config.height
+		return wrapper
+	},
+
+	readFileTrick: function (url, callback) {
+		var xmlHttp = new XMLHttpRequest()
+		var self = this
+		xmlHttp.onreadystatechange = function() {
+			if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+				console.log("EEE!")
+				self.config.content = xmlHttp.responseText
+				self.updateDom()
+			}
+		}
+		xmlHttp.open("GET", url, true)
+		xmlHttp.send(null)
+	}
 })
